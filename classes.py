8537cd52f32579738dfc44224b885f94f2cdfa75
Kyllo Brooks <kyllobrooks@gmail.com>
2023-01-15 01:20:13 -0600

    moved classes into own file

--- classes.py
+++ classes.py
@@ -3,36 +3,6 @@ import json
 import numpy as np
 import requests
 
-# fetch the secrets from the keyfile
-keysJSON = open('keys.json')
-open_file = keysJSON.read()
-model_attribs = json.loads(open_file)['model']
-keys = json.loads(open_file)['reddit']
-keysJSON.close()
-
-# setup api connection variables
-api_key = model_attribs['apikey']
-api_url = "https://kyllobrooks.com/api/mhs"
-
-# fetch and construct praw object
-reddit = praw.Reddit(
-    client_id = keys['client_id'],
-    client_secret = keys['client_secret'],
-    password = keys['password'],
-    user_agent="web:mhs-crawler-bot:v1 (by /u/mhs-crawler-bot)",
-    username="mhs-crawler-bot",
-)
-
-# set readonly mode
-reddit.read_only = False
-
-
-# # fill the list with the top subreddits in descending order of subscriber numbers
-# subs = []
-# for subreddit in reddit.subreddits.popular(limit=None):
-#     subs.append(subreddit.display_name)
-
-# define a class that has a method in its constructor to build ordered lists of mods, comments, and commentors
 class Sub:
     """
     Class object representing a subreddit
@@ -195,11 +165,3 @@ class Sub:
     def mod_set(self):
         """Returns a python set of moderators"""
         return self.__mod_set
-
-# example code to build a sub object from the subreddit display name 'cork' i think this is county cork in ireland
-cork = Sub('cork', api_url, api_key, reddit, inference=False)
-
-# you can interact with this object with its accessor methids for mods, samples, and author
-print(cork.stats())
-cork.infer()
-print(cork.stats())
---
+++ main.py
@@ -0,0 +1,37 @@
+import praw
+import json
+import numpy as np
+import requests
+
+from classes import Sub
+
+# fetch the secrets from the keyfile
+keysJSON = open('keys.json')
+open_file = keysJSON.read()
+model_attribs = json.loads(open_file)['model']
+keys = json.loads(open_file)['reddit']
+keysJSON.close()
+
+# setup api connection variables
+api_key = model_attribs['apikey']
+api_url = "https://kyllobrooks.com/api/mhs"
+
+# fetch and construct praw object
+reddit = praw.Reddit(
+    client_id = keys['client_id'],
+    client_secret = keys['client_secret'],
+    password = keys['password'],
+    user_agent="web:mhs-crawler-bot:v1 (by /u/mhs-crawler-bot)",
+    username="mhs-crawler-bot",
+)
+
+# set readonly mode
+reddit.read_only = False
+
+# example code to build a sub object from the subreddit display name 'cork' i think this is county cork in ireland
+cork = Sub('cork', api_url, api_key, reddit, inference=False)
+
+# you can interact with this object with its accessor methids for mods, samples, and author
+print(cork.stats())
+cork.infer()
+print(cork.stats())
