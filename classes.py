243abf4924e63cace80b04441f2fe1bdad127d57
Kyllo Brooks <kyllobrooks@gmail.com>
2023-01-17 20:24:22 -0600

    fixed the failing tests for Sub class

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import requests
 from tqdm import tqdm
+import datetime
 
 # TODO: Need a class or function for the object describing the matrix of relationships between subs, maybe a sparse matrix
 # TODO: Need a class or function to handle pushing the data into a database
@@ -26,10 +27,11 @@ class Sub:
     """
     # defines a constructor
     def __init__(self, display_name, url, apikey, praw_object, inference=True, forest_width=10, per_post_n=100, comments_n=1000, chunk_size=50):
-        # public members
-        self.name = display_name
-        self.mhs_score = 0.0
         # private members
+        self.__results = []
+        self.__stats = {}
+        self.__name = display_name
+        self.__date = datetime.datetime.now()
         self.__mods_list = []
         self.__sub = praw_object.subreddit(display_name)
         self.__posts = self.__sub.top(time_filter="week", limit=None)
@@ -41,56 +43,69 @@ class Sub:
         self.__chunksize = chunk_size
         self.__sample_list = []
         self.__author_list = []
-        self.__results_list = []
-        self.__stats = [0,0,0,0]
+        self.__nested_results_list = []   
         self.__mod_set = {}
         self.__author_set = {}
+        self.__comment_list = []
         # constructors and sanitizers
-        self.__build_mods()
-        self.__build_content()
-        self.__sanitize()
+        self.__fetch_mods()
+        self.__fetch_content()
+        self.__build_lists()
         self.__build_sets()
         if inference == True:          
             self.__get_mhs_ratings(self.__chunksize)
+            self.__results = self.__flatten_results(self.__nested_results_list)
+            self.__results = list(self.__results)
             self.__build_stats()
 
     # constructor function to build moderator list
-    def __build_mods(self):
+    def __fetch_mods(self):
         for mod in self.__sub.moderator():
             self.__mods_list.append(mod.name)
 
     # constructor function to build comments list
-    def __build_content(self):
-        for post in self.__posts:
-            if len(self.__sample_list) >= self.__comments_n:
-                break
-            post_comment_count = 0
+    def __fetch_content(self):
+        t = tqdm(total=self.__comments_n, desc='Collection: '+ self.__name)
+        post_tick = 0
+        for post in self.__posts:      
             post.comments.replace_more(limit=self.__forest_width)
-            for comment in post.comments.list():
-                if post_comment_count >= self.__per_post_n:
+            comments = post.comments.list()
+            comment_tick = 0
+            for comment in comments:
+                if comment_tick >= self.__per_post_n: 
                     break
-               
-                try:                    
-                    self.__sample_list.append(comment.body)
-                except AttributeError:
-                    self.__sample_list.append('empty_string')
-                try:
-                    self.__author_list.append(comment.author.name)
-                except AttributeError:
-                    self.__author_list.append('empty_string')
- 
-                post_comment_count += 1
+                if comment.author is not None:
+                    t.update(1)
+                    data = {
+                        'body': self.__sanitize(comment.body),
+                        'author': comment.author.name
+                    }
+                    self.__comment_list.append(data)
+                    if len(self.__comment_list) == self.__comments_n:
+                        break
+                    comment_tick += 1
+                    #print('post :' + str(post_tick) + ' comment tick :' + str(comment_tick))
+            if len(self.__comment_list) == self.__comments_n:
+                break
+            post_tick += 1
 
-     
+       # defines a function to build lists from a dict
+    def __build_lists(self):
+        for data in self.__comment_list:
+            self.__sample_list.append(data['body'])
+            self.__author_list.append(data['author'])
+
+
     # defines a funtion to clean text in the strings from weird chars
-    def __sanitize(self):
-        self.__sample_list[:] = [item for item in self.__sample_list if item.strip() == item]
-        self.__sample_list[:] = [item for item in self.__sample_list if item.strip('\n') == item]
-        self.__sample_list[:] = [item for item in self.__sample_list if item.replace('\n','') == item]
-        self.__sample_list[:] = [item for item in self.__sample_list if item.replace('\n\n','') == item]
-        self.__sample_list[:] = [item for item in self.__sample_list if item.replace('\\','') == item]
-        self.__sample_list[:] = [item for item in self.__sample_list if item.replace('/','') == item]
-    
+    def __sanitize(self, item):
+        item = item.strip()
+        item = item.strip('\n')
+        item = item.replace('\n','')
+        item = item.replace('\n\n','')
+        item = item.replace('\\','')
+        item = item.replace('/','')
+        return item
+   
     # defines a funtion to perform inference on the chunks
     def __request_inferance(self, instances, apikey, url):
         payload = { "instances": instances }
@@ -104,26 +119,25 @@ class Sub:
         return zip(*[iter(iterable)] * chunksize)
 
     # defines a helper function to flatten the results
-    def __flatten(self, predictions):
+    def __flatten_results(self, predictions):
         for item in predictions:
             try:
-                yield from self.__flatten(item)
+                yield from self.__flatten_results(item)
             except TypeError:
                 yield item
 
     # defines a function that carries out inference on chunks of N samples
     def __get_mhs_ratings(self, N):
-        for chunk in tqdm (self.__chunker(self.__sample_list, N), total= len(self.__sample_list)/self.__chunksize, desc='Inference: '+ self.name):
+        for chunk in tqdm (self.__chunker(self.__sample_list, N), total= len(self.__sample_list)/self.__chunksize, desc='Inference: '+ self.__name):
             while True:
                 try:
                     response = self.__request_inferance(chunk, self.__apikey, self.__url)
-                    self.__results_list.append(response['predictions'])
+                    self.__nested_results_list.append(response['predictions'])
                 except requests.exceptions.HTTPError as e:
                     # could add a logging call here for the error 'e'
                     continue
                 break
             pass
-        return list(self.__flatten(self.__results_list))
 
     # defines a function to build unordered sets from author and mod list members
     def __build_sets(self):
@@ -132,21 +146,32 @@ class Sub:
 
     # builds an array of descriptive statistics for the results
     def __build_stats(self):
-        arr = np.array(self.__results_list)
-        self.__stats[0] = arr.min()
-        self.__stats[1] = arr.max()
-        mean = arr.mean()
-        self.__stats[2] = mean
-        self.mhs_score = mean
-        self.__stats[3] = arr.std()
+        arr = np.array(self.__results)
+        self.stats = {
+            'min': arr.min(),
+            'max': arr.max(),
+            'mean': arr.mean(),
+            'std': arr.std()
+        }
 
     # defines a convenience function to run inference after the object already exists or if it was created with inference=False
     def infer(self):
         """Convenience method to carry out inference on the Sub after it has been constructed with inference=False"""
         self.__get_mhs_ratings(self.__chunksize)
+        self.__results = self.__flatten_results(self.__nested_results_list)
+        self.__results = list(self.__results)
         self.__build_stats()
 
     # Access functions
+
+    def name(self):
+        """Returns the display name of the subreddit"""
+        return self.__name
+
+    def date(self):
+        """Returns the date of sample gathering"""
+        return self.__date
+    
     def mods(self):
         """Returns the list of moderators"""
         return self.__mods_list
@@ -154,17 +179,17 @@ class Sub:
     def samples(self):
         """Returns the list of text samples gathered"""
         return self.__sample_list
-    
-    def results(self):
-        """Returns the list of inference results"""
-        return self.__results_list
 
     def authors(self):
         """Returns the list of comment authors"""
         return self.__author_list
+
+    def results(self):
+        """Returns a list of inference results"""
+        return self.__results
     
     def stats(self):
-        """Returns an array of descriptive stats: [min, max, mean, std]"""
+        """Returns a dict of descriptive stats: [min, max, mean, std]"""
         return self.__stats
     
     def author_set(self):
