cf2202d13b1163729079e132464f6e1f5fc5d229
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-01-24 00:57:38 -0600

    Basic Model Structure Setup
    created the basic model structures needed to store a test snapshot and the node + edge tables
    
    made sure to comment things nicely.
    
    Still a lot left to implement and refine.

--- toxit_main/toxit_main/settings.py
+++ toxit_main/toxit_main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'toxit.apps.ToxitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Canada/Saskatchewan' # where the hell is this?
 
 USE_I18N = True
 
