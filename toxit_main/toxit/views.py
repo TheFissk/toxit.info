1aad6c4bc41ea7ac10095ec991af78c17395b87b
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-01-24 12:12:53 -0600

    setup for visjs
    navigating to /toxit/ draws a view, but visjs is not installed yet
    
    making a commit of this current state incase the install goes horribly wrong and I need to reset this

--- toxit_main/toxit/views.py
+++ toxit_main/toxit/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
+# from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    template = loader.get_template('toxit/index.html')
+    return HttpResponse(template.render())
