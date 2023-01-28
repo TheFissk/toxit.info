eb46e1ac5719ee932df99e872451b86e1c0974a5
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-01-28 17:32:08 -0600

    VisJS displays data from database
    VisJS is working and grabbing data from the django DB succesfully

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Front End Development 
 
-### Description 
-For now this is a basic outline of our project. Currently this is using SQLite3 for the DB since it is native to Django and will work well with the test development servers we can host on our local machines. 
+### Front End - To Do:
+ - [x] create toxit django app
+ - [x] install VisJS 
+ - [x] get VisJS working in Djano
+ - [x] get toxit app to boiler plate stage 
+      - models are reduced to just node and edge for testing
+      - VisJs is installed 
+ - [x] get VisJS to display data from database
+ - [ ]  ~~get Edge model to validate against reversed pairs ~~ put on hold
+       - doubling edges may allow for label at each node connection point
+ - [ ] create side menu using template engine
+ - [ ] integrate side menu into main view
--- toxit_main/toxit/models.py
+++ toxit_main/toxit/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 class Node(models.Model):
     subreddit_name = models.CharField(max_length=24) # regex = r"^([a-z0-9][_a-z0-9]{2,20})$"\n
     toxicity_score = models.FloatField(default=0)
@@ -16,13 +17,22 @@ class Edge(models.Model):
     shared_mod_sim = models.FloatField(default=0, help_text="Arc weight representing some magic stats number with a fancy name.")
 
     class Meta:
-        unique_together = ('start_node', 'end_node')
-    
+        unique_together = ['start_node', 'end_node']
+
     def validate_unique(self, exclude=None):
         if self.start_node == self.end_node:
             raise ValidationError("Node cannot refer to self.")
         super(Edge, self).validate_unique(exclude=exclude)
 
+    def validate_reverse(self, exclude=None):
+        test1 = Node.objects.get(pk=self.start_node.id)
+        test2 = Node.objects.get(pk=self.end_node.id)
+        if Edge.objects.filter(start_node=test2, end_node=test1):
+            raise ValidationError("Reversed edge already exists.")
+        super(Edge, self).validate_reverse(exclude=exclude)
+
+    def get_vis_edge(self):
+        return str("from: " + str(self.start_node.id) + ", to: " + str(self.end_node.id))
+
     def __str__(self):
-        return str("from: " + self.start_node.subreddit_name + ", to: " + self.end_node.subreddit_name)
-        
+        return str("from: " + self.start_node.subreddit_name + ", to: \"" + self.end_node.subreddit_name)
