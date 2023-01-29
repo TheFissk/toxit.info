43d38d31eb0ad60033e4c162abd5ad72c28c42d1
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2023-01-28 22:51:23 -0600

    Merging files

--- toxit_main/toxit/views.py
+++ toxit_main/toxit/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'main/sidebar.html')
+from .models import Node, Edge
 
+def index(request):
+    template = loader.get_template('toxit/index.html')
+
+    nodes = Node.objects.all()
+    edges = Edge.objects.all()
+
+    context = {
+        'nodes': nodes,
+        'edges': edges,
+    }
+
+    return render(request, 'toxit/index.html', context)
