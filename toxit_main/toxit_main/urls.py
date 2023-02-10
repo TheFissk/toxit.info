71feeb2c9abf70f994fc65adc499e96bfa782f02
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-02-10 13:05:23 -0600

    Stable Coldfix - Running Again
    Stable was unable to runserver completely; the app now works again and test environment can be run again.
    
    The main issue was some failed migrations stuck in pycache and a django app that doesn't exist listed in INSTALLED_APPS in toxit_main/settings.py
    
    also removed an additional folder that doesn't seem to be necessary for anything at all
    
    I also added in a second url pattern in toxit_main/paths.py with custom namespaces so you don't have to add '/toxit/' to the test server ip anymore

--- toxit_main/toxit_main/urls.py
+++ toxit_main/toxit_main/urls.py
@@ -1,23 +1,20 @@
 """toxit_main URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    The `urlpatterns` list routes URLs to views https://docs.djangoproject.com/en/4.1/topics/http/urls/
+
+    namespace is needed to have multiple urls link to the langing page,
+    if no namepsace is provided in the include() portion of path then the warning
+
+        WARNINGS:
+        ?: (urls.W005) URL namespace 'toxit' isn't unique. You may not be able to reverse all URLs in this namespace
+
+    gets raised and can cause side effects later in development 
 """
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('', include('toxit.urls')), # no url redirect instead of /toxit/
-    path('toxit/', include('toxit.urls')),
+    path('', include('toxit.urls', namespace='toxit_no_url')), # no url redirect instead of /toxit/
+    path('toxit/', include('toxit.urls', namespace='toxit')),  # namespace necessary to keep each unique and prevent W005 error
     path('admin/', admin.site.urls),
 ]
