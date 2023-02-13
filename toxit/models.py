0d2ae203a9bc2d21078af2d7ee65d8c8cbe57311
Tim Fiss <timothy.fiss@gmail.com>
2023-02-12 19:01:21 -0600

    stump removed

--- toxit/models.py
+++ toxit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+import json
 '''
 DONE:
 - Check ERD for correctness, https://drawsql.app/teams/kyllo-brooks-digital-services/diagrams/reddit-mhs-erd-2
@@ -19,15 +20,16 @@ LAST:
 - Lock ORM Models
 '''
 
+
 class Subreddit(models.Model):
     class Meta:
         db_table = 'toxit_subreddit'
 
     custom_id = models.CharField(primary_key=True, max_length=20, unique=True,
-        help_text="Custom key from reddit")
+                                 help_text="Custom key from reddit")
 
     display_name = models.CharField(max_length=32,
-        help_text="Display name of the subreddit")
+                                    help_text="Display name of the subreddit")
 
     def __str__(self):
         return self.display_name
@@ -37,94 +39,100 @@ class Inference_task(models.Model):
     class Meta:
         db_table = 'toxit_inference_task'
 
-    TIME_SCALES =   [
-                    ('hour', 'This Hour'),
-                    ('day', 'Today'),
-                    ('week', 'This Week'),
-                    ('month', 'This Month'),
-                    ('year', 'This Year'),
-                    ('all', 'All Time')
-                    ]
+    TIME_SCALES = [
+        ('hour', 'This Hour'),
+        ('day', 'Today'),
+        ('week', 'This Week'),
+        ('month', 'This Month'),
+        ('year', 'This Year'),
+        ('all', 'All Time')
+    ]
 
     STATUS_TYPES = [
-                    ('0', 'Scheduled'),
-                    ('1', 'In Progress'),
-                    ('2', 'Completed'),
-                    ('3', 'Error'),
-                    ('4', 'Cancelled')
-                    ]
+        ('0', 'Scheduled'),
+        ('1', 'In Progress'),
+        ('2', 'Completed'),
+        ('3', 'Error'),
+        ('4', 'Cancelled')
+    ]
 
     start_sched = models.DateTimeField(
         help_text='Requested start time')
-    
+
     time_scale = models.CharField(max_length=5, choices=TIME_SCALES,
-        help_text="The period overwhich the comments will be harvested")
+                                  help_text="The period overwhich the comments will be harvested")
 
     min_words = models.IntegerField(default=20, null=True, validators=[MinValueValidator(1), MaxValueValidator(168)],
-        help_text="The minimum number of words to be considered a valid comment")
+                                    help_text="The minimum number of words to be considered a valid comment")
 
     forest_width = models.IntegerField(default=10, validators=[MinValueValidator(0)],
-        help_text="The number of comments per level to be acquired")
+                                       help_text="The number of comments per level to be acquired")
 
     per_post_n = models.IntegerField(default=1000, validators=[MinValueValidator(1)],
-        help_text="The maximum number of comments per post to be harvested")
+                                     help_text="The maximum number of comments per post to be harvested")
 
     comments_n = models.IntegerField(default=1000, validators=[MinValueValidator(1)],
-        help_text="The number of comments to be harvested")
+                                     help_text="The number of comments to be harvested")
 
     subreddit_set = models.JSONField(
         help_text="A set of the subreddits to be harvested")
 
     status = models.PositiveSmallIntegerField(choices=STATUS_TYPES,
-        help_text="The status of the task")
+                                              help_text="The status of the task")
 
     def get_subreddits_for_inference_task(self):
         return Subreddit_result.objects.filter(inference_task=self)
 
-    def get_unique_edge_pairs(self):
-        edge_pairs = []
-        
-        subreddit_list = self.get_subreddits_for_inference_task
-        
+    def get_unique_edge_pairs(self) -> tuple[list[tuple], list[tuple]]:
+        """
+        Returns the edge pairs for these nodes as a list of tuples. 
+
+        In the form:(From_Sub, To_Sub, Strength)
+        """
+        modPairs = []
+        authorPairs = []
+        subreddit_list = self.get_subreddits_for_inference_task()
         for sub in subreddit_list:
-            edges = sub.edges
-            for edge in edges:
-                from_node = edge['from']
-                to_node = edge['to']
-                # Check if the edge pair already exists in the list or its reverse
-                if (from_node, to_node) not in edge_pairs and (to_node, from_node) not in edge_pairs:
-                    edge_pairs.append((from_node, to_node))
-        return edge_pairs
+            from_Sub = sub.subreddit.display_name
+            edges = json.loads(sub.edges)
+            for to_Sub in edges['mods'].keys():
+                modPairs.append((from_Sub, to_Sub, edges['mods'][to_Sub]))
+
+            for to_Sub in edges['authors'].keys():
+                authorPairs.append(
+                    (from_Sub, to_Sub, edges['authors'][to_Sub]))
+            # no need to check for uniqueness, the property is guarenteed
+        return modPairs, authorPairs
 
     def __str__(self):
         if (self.start_sched):
             return f"Inference job scheduled: {self.start_sched}"
- 
+
 
 class Subreddit_result(models.Model):
     class Meta:
         db_table = 'toxit_subreddit_result'
 
     subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE,
-        help_text="The subreddit that was analyzed")
+                                  help_text="The subreddit that was analyzed")
 
     inference_task = models.ForeignKey(Inference_task, on_delete=models.CASCADE,
-        help_text="The inference task in which this data was collected")
+                                       help_text="The inference task in which this data was collected")
 
     min_result = models.FloatField(blank=True, null=True,
-        help_text="The minimum score from this subreddit")
+                                   help_text="The minimum score from this subreddit")
 
     max_result = models.FloatField(blank=True, null=True,
-        help_text="The maxmimum score from this subreddit")
+                                   help_text="The maxmimum score from this subreddit")
 
     mean_result = models.FloatField(blank=True, null=True,
-        help_text="The average score from this subreddit")
+                                    help_text="The average score from this subreddit")
 
     std_result = models.FloatField(blank=True, null=True,
-        help_text="The standard deviation for this subreddit")
+                                   help_text="The standard deviation for this subreddit")
 
     timestamp = models.DateTimeField(auto_now_add=True,
-        help_text="The time when the data was collected")
+                                     help_text="The time when the data was collected")
 
     edges = models.JSONField(
         help_text="The edges for this subreddit")
@@ -140,11 +148,10 @@ class Subreddit_mod(models.Model):
     subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
 
     subreddit_result = models.ForeignKey(Subreddit_result, on_delete=models.CASCADE,
-        help_text="The collection that the user was a moderator during")
+                                         help_text="The collection that the user was a moderator during")
 
     username = models.CharField(max_length=32,
-        help_text="The username of the moderator")
-
+                                help_text="The username of the moderator")
 
     def __str__(self):
         return f"User: {self.username}, Subreddit: {self.subreddit_result.subreddit}"
@@ -154,22 +161,22 @@ class Comment_result(models.Model):
     class Meta:
         db_table = 'toxit_comment_result'
 
-    subreddit_result = models.ForeignKey(Subreddit_result, on_delete=models.CASCADE)
+    subreddit_result = models.ForeignKey(
+        Subreddit_result, on_delete=models.CASCADE)
 
     subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
 
     permalink = models.TextField(
         help_text='The permalink to the comment sample')
 
-    mhs_score = models.FloatField(default=0, 
-        help_text='The mhs inference score of the sample')
+    mhs_score = models.FloatField(default=0,
+                                  help_text='The mhs inference score of the sample')
 
     comment_body = models.TextField(
         help_text='The comment sample')
 
-    username = models.CharField(max_length=32, 
-        help_text='The username of the commentor')
-
+    username = models.CharField(max_length=32,
+                                help_text='The username of the commentor')
 
     def __str__(self):
         return f"Post By: {self.username} in: {self.subreddit.display_name}.\nScore: {self.mhs_score}.\nText: {self.comment_body}"
