33f81c06c687cf30d9e9f709e320d78e689a1ce5
Tim Fiss <timothy.fiss@gmail.com>
2023-02-13 22:46:17 -0600

    Its scuffed but it works

--- toxit/views.py
+++ toxit/views.py
@@ -5,18 +5,17 @@ from django.shortcuts import render
 from .models import Inference_task, Subreddit_result
 
 def index(request):
-    IT = Inference_task.objects.get(pk=21)
+    IT = Inference_task.objects.get(pk=22)
     Subreddits = IT.get_subreddits_for_inference_task()
-    EdgeTable = IT.get_unique_edge_pairs()
+    mod_edge, author_edge = IT.get_unique_edge_pairs()
 
     edge_data = []
-    for edge in EdgeTable:
+    for edge in mod_edge:
         edge_data.append({
-            'from_Sub': edge[0][0],
-            'to_Sub': edge[0][1],
-            'label': edge[0][2]
+            'from_Sub': edge[0],
+            'to_Sub': edge[1],
+            'label': edge[2]
         })
-
     context = {
         'subs': Subreddits,
         'edge_data': edge_data,
