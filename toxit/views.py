4cd2bbe8cf561d511b7a179d4641967f95f8bff6
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-02-20 17:43:41 -0600

    quicksave

--- toxit/views.py
+++ toxit/views.py
@@ -9,13 +9,15 @@ def update_data(request, snapshot_id):
     snapshot = get_object_or_404(Inference_task, id=snapshot_id)
 
     # Get the Subreddit_results for the selected snapshot
-    subreddit_results = snapshot.get_subreddits_for_inference_task()
+    fetched_subs = snapshot.get_subreddits_for_inference_task()
+    fetched_mods = snapshot.get_mod_edges_for_inference_task()
+    fetched_authors = snapshot.get_author_edges_for_inference_task()
 
     # Prepare the data to be returned
-    sub_nodes_context = [(result.subreddit.name, result.subreddit.name) for result in subreddit_results]
-    mod_edges_context = [(result.subreddit.name, result.min_result) for result in subreddit_results if result.min_result is not None]
-    autor_edges_context = [(result.subreddit.name, result.max_result) for result in subreddit_results if result.max_result is not None]
-    data = {'sub_nodes_context': sub_nodes_context, 'mod_edges_context': mod_edges_context, 'autor_edges_context': autor_edges_context}
+    sub_nodes_context = [(result.subreddit.name, result.subreddit.name) for result in fetched_subs]
+    mod_edges_context = [(result.subreddit.name, result.min_result) for result in fetched_mods]
+    author_edges_context = [(result.subreddit.name, result.max_result) for result in fetched_authors]
+    data = {'sub_nodes_context': sub_nodes_context, 'mod_edges_context': mod_edges_context, 'autor_edges_context': author_edges_context}
 
     # Return the data as a JSON response
     return JsonResponse(data)
@@ -23,14 +25,15 @@ def update_data(request, snapshot_id):
 def index(request):
     template = loader.get_template('toxit/index.html')
 
-    # all 
+    # grab all of the inference tasks
     iTasks = Inference_task.objects.all()
 
-    # Get the selected snapshot
+    # Get the selected inference task or snapshot; default first for now
     selected = Inference_task.objects.first()
 
     context = {
         'iTasks': iTasks,
+        'selected': selected,
     }
 
     return render(request, 'toxit/index.html', context)
