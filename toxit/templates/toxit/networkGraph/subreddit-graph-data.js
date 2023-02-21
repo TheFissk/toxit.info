3b3f4095d4d44c93a59b2649158ae22b9b01c2e5
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-02-21 17:42:46 -0600

    onload and formatting changes
    ajax now called on load for first item in list since that is what displays by default in the select
    
    I did some style changes to move the snapshot select out of the way and overtop of things for the time being, it should be easy to add some simple css to create a rounded box around it like the menu toggle switch has

--- toxit/templates/toxit/networkGraph/subreddit-graph-data.js
+++ toxit/templates/toxit/networkGraph/subreddit-graph-data.js
@@ -1,1 +1,83 @@
 // console.log("networkGraph.js loaded!");
+
+var sub_nodes = new vis.DataSet();
+var mod_edges = new vis.DataSet();
+var author_edges = new vis.DataSet();
+
+var container = document.getElementById("Toxit-SubredditGraph");
+
+var data = {
+  nodes: sub_nodes,
+  edges: mod_edges,
+};
+
+var options = {
+    nodes: {
+      shape: "circle",
+      margin: 10,
+      size: 16,
+    },
+    layout: {
+      randomSeed: 69,
+    },
+    physics: {
+      solver: "forceAtlas2Based",
+      maxVelocity: 50,
+      timestep: 0.35,
+      stabilization: {
+        enabled: true,
+        iterations: 1000,
+        updateInterval: 25,
+      },
+      forceAtlas2Based: {
+        gravitationalConstant: -30,
+        centralGravity: 0.01,
+        springLength: 250,
+        springConstant: 0.05,
+      },
+    },
+    clustering: {
+      enabled: true,
+      clusterNodeProperties: {
+        borderWidth: 2,
+        shape: "box",
+        font: { size: 20 },
+      },
+    },
+  };  
+
+var network = new vis.Network(container, data, options);
+
+// Define a function to update the graph data
+function updateGraphData(snapshot_id) {
+  var url = '/update_data/' + snapshot_id + '/';
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      // Clear the existing data
+      sub_nodes.clear();
+      mod_edges.clear();
+      author_edges.clear();
+
+      // Add the new data
+      sub_nodes.add(data.sub_nodes_context);
+      mod_edges.add(data.mod_edges_context);
+      author_edges.add(data.author_edges_context);
+    },
+    error: function(xhr, status, error) {
+      console.log('Error:', error);
+    }
+  });
+}
+
+// Call the function to update the graph data for the first choice on page load
+var firstChoiceValue = $('#snapshot-select option:first').val();
+updateGraphData(firstChoiceValue);
+
+// Add event listener to snapshot-select select tag
+document.getElementById('snapshot-select').addEventListener('change', function() {
+  var snapshot_id = this.value;
+  updateGraphData(snapshot_id);
+});
