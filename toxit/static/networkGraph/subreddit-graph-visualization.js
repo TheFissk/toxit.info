a462f2ac5fecdc49123751388cfe2e2939f3cc2b
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2023-02-21 14:21:10 -0600

    Added the important bits from dynamic edges branch

--- toxit/static/networkGraph/subreddit-graph-visualization.js
+++ toxit/static/networkGraph/subreddit-graph-visualization.js
@@ -14,3 +14,36 @@
 // };
 
 // var network = new vis.Network(container, data, options);
+
+
+
+// Functions to add/remove edges/Nodes
+//
+//var button22 = document.getElementById('mybutton');
+//button22.addEventListener('click', addNode);
+//
+//var button33 = document.getElementById('mybutton2');
+//button33.addEventListener('click', removeAllEdges);
+//
+//      function addNode() {
+//        var newId = (Math.random() * 1e7).toString(32);
+//        nodes.add({ id: newId, label: "I'm new!" });
+//        var edge = { from: newId, to: 1, id: (Math.random() * 1e7).toString(32)};
+//        edges.add(edge);
+//      }
+//       function removeAllEdges() {
+//            console.log("remove all");
+//            edges.clear();
+//      }
+//
+//function addEdge(edge) {
+//        try {
+//          edges.add({
+//            id: edge.id.value,
+//            from: edge.from.value,
+//            to: edge.to.value,
+//          });
+//        } catch (err) {
+//          alert(err);
+//        }
+//      }
