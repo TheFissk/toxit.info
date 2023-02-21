c7e50fbaaf8e616ecdfb36b26d9c05e8037d34f1
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2023-02-21 16:06:01 -0600

    Slightly better
    Visjs still refuses to take up the screen

--- toxit/templates/toxit/networkGraph/subreddit-graph-data.js
+++ toxit/templates/toxit/networkGraph/subreddit-graph-data.js
@@ -1,1 +1,23 @@
 // console.log("networkGraph.js loaded!");
+
+
+//        nodes.add({ id: newId, label: "I'm new!" });
+//        var edge = { from: newId, to: 1, id: (Math.random() * 1e7).toString(32)};
+//        edges.add(edge);
+//      }
+//       function removeAllEdges() {
+//            console.log("remove all");
+//            edges.clear();
+//      }
+//
+//function addEdge(edge) {
+//        try {
+//          edges.add({
+//            id: edge.id.value,
+//            from: edge.from.value,
+//            to: edge.to.value,
+//          });
+//        } catch (err) {
+//          alert(err);
+//        }
+//      }
