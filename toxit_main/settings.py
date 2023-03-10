60b4ddf4979a1d41ff288f600863edbbb476b2e0
Kyllo Brooks <kyllobrooks@gmail.com>
2023-03-10 17:34:41 -0600

    toggle debugging

--- toxit_main/settings.py
+++ toxit_main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q)01061hj=ax=+yw&vs0zt(ef)t7+ce5e*y&@_e)@sy08_4m@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
